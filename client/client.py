85b9795e264ec3b0ffcdf08688b6654f87afb8ce
SousaThiago749 <thiagoggs@al.insper.edu.br>
2023-09-15 11:08:02 -0300

    avanco client

--- client/client.py
+++ client/client.py
@@ -94,6 +94,10 @@ def main():
         for i in range(0, len(txBuffer), tam_pacote):
             pacote = txBuffer[i:i + tam_pacote]
             pacotes.append(pacote)
+        
+        print('\n\n')
+        print(pacotes)
+        print('\n\n')
 
 
         hs = 0
@@ -106,90 +110,33 @@ def main():
         
         tamanho_pacotes = len(pacotes)
         tamanho_pacotes_bytes = tamanho_pacotes.to_bytes(1, byteorder='big')  # Use 4 bytes para representar o tamanho
-        print(tamPacotesBytes)
+        #print(tamPacotesBytes)
+
+
 
         print('aaaaaaaaaaaaaaaaaaaaaaa')
         ki = 0
         for pacote in range(len(pacotes)-1):
-            envio = tamPacotesBytes + b'\x00\x00\xBB\x00' + b'\xBB\x00\x00\x00' + pacotes[pacote] + eop_inicio
+            #envio = tamPacotesBytes + b'\x00\x00\xBB\x00' + b'\xBB\x00\x00\x00' + pacotes[pacote] + eop_inicio
+            envio = pacotes[pacote] 
             print(envio)
-            time.sleep(0.2)
             com1.sendData(envio)
-            time.sleep(2)
-            ki+1
+            time.sleep(3)
+            ki +=1
             print(ki)
-    
+        
+        time.sleep(3)
         envio = tamPacotesBytes + b'\xAA\xAA\xAA\xAA' + b'\xBB\x00\x00\x00' + pacotes[-1] + eop_inicio
-        time.sleep(0.2)
+        print('\n\n\n\n\n')
+        print(envio)
+        print(ki+1)
+        time.sleep(3)
         com1.sendData(envio)
         time.sleep(2)
 
-
-
-
-        # while ESTADO == hs:
-        #     timeout = time.time() + 5
-        #     if len(tamanho) == 0 or time.time()>timeout:
-        #         quest = input(str("Servidor inativo. Tentar novamente? s/n : "))
-        #         if quest == 's':
-        #             com1.sendData(handshake)
-        #             time.sleep(0.1)
-        #             tamanho, nRx = com1.getData(1)
-        #             print(tamanho==tamPacotesBytes)
-        #         elif quest == 'n':
-        #             print("Comunicação encerrada")
-        #             com1.disable()
-
-        #     elif tamanho == tamPacotesBytes and time.time()<timeout:
-        #         print("Tamanho do arquivo recebido com sucesso")
-        #         ESTADO = envio
-        #         break
-
-        # while ESTADO == envio:
-        #     for pacote in range(len(pacotes)):
-        #         print(pacote)
-        #         # ENVIAR PACOTE
-        #         com1.sendData(pacote)
-
-
-
-        # # Byte de inicio
-        # time.sleep(.2)
-        # com1.sendData(handshake)
-        # time.sleep(2)
-
-        # # Recebendo o Byte de inicio
-        # print("esperando 1 byte de resposta")
-        # tamanho, nRx = com1.getData(1)
-
-        # com1.rx.clearBuffer()
-        # time.sleep(.1)
-        
-
-
-
-        # def checar():
-        #     #“Servidor inativo. Tentar novamente? S/N”
-        #     timeout = time.time() + 5
-        #     #time.sleep(6)
-        #     if len(tamanho) != 0 and time.time()<timeout:
-        #         print('a')
-        #     else:
-        #         questao = input(str("Servidor inativo. Tentar novamente? S/N : " ))
-        #         if questao == "N":
-        #             # Encerra comunicação
-        #             print("-------------------------")
-        #             print("Comunicação encerrada")
-        #             print("-------------------------")
-        #             com1.disable()
-        #         else:
-        #             # Byte de inicio novamente
-        #             time.sleep(.2)
-        #             com1.sendData(b'\x00')
-        #             time.sleep(1)
-
-
-
+        time.sleep(3)
+        com1.sendData(final)
+        time.sleep(2)
         
         # # Encerra comunicação
         # print("-------------------------")
